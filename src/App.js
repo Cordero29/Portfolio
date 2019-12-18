722da7275c20a3dec4631eb20a5ba3b4fa4f8229
Cordero <ChristopherCordero29@gmail.com>
2019-12-18 14:56:36 -0500

    Hello World

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Animation from "./components/Animation";
+import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
+import Portfolio from "./components/Portfolio";
+import ContactMe from "./components/ContactMe";
+import Footer from "./components/Footer";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Animation />
+        <Header />
+        <AboutMe />
+        <Portfolio />
+        <ContactMe />
+        <Footer />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header id="header">
+      <nav>
+        <ul>
+          <a href="#">
+            <li>Home</li>
+          </a>
+          <a href="#aboutMe">
+            <li>About Me</li>
+          </a>
+          <a href="#portfolio">
+            <li>Portfolio</li>
+          </a>
+          <a href="#contactMe">
+            <li>Contact Me</li>
+          </a>
+          <a href="https://docdro.id/TeE0qHU" target='_blank'>
+            <li>Resume</li>
+          </a>
+        </ul>
+      </nav>
+    </header>
+  );
+}
